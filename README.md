f0f946fae7a3f3d2d769e11ddf3c98bd5918025f
arzkar <arzkar.dev@gmail.com>
2021-02-06 22:37:47 +0530

    Added ffnet and ao3 series support
    - main.py: Added regex search query for ffnet
    - metadata_processing.py: To process ao3_metadata_works and ao3_metadata_series
    - metadata.py: Added ffnet searching and added cloudscraper to scrape ffnet
    - processing.py: Added functions to efficiently process ffnet metadata
    - search.py: Replaced get_fic_id() with get_ao3_id() and get_ffn_id()

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 In this project, I created a discord bot written in Python which searches google for a fanfiction and parses the html into readable metadata and send the metadata as an embed message. <br>
 # Bot Usage
-Use `ao3 fic_name` to search a fanfiction.<br>
+Use `ao3 fic_name` or `ao3 fic_name series` and `ffn fic_name` to search a fanfiction <br>
+U can also use the url of the fanfiction for searching
 The query string is not case-sensitive so either uppercase, lowercase or combination of both can be used to search.<br>
 The following is an example on how the bot works in realtime-<br>
 
--- utils/search.py
+++ utils/search.py
@@ -2,45 +2,102 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def get_fic_id(query):
+
+def get_ao3_id(query):
     ao3_list = []
     pos_of_slash1 = []
     ao3_id_list1 = []
+    hrefs = []
     pos_of_qmark = None
+
     url = 'https://www.google.com/search?q=' + \
         query+"+ao3"
-    page = requests.get(url) 
+
+    page = requests.get(url)
+
     soup = BeautifulSoup(page.content, 'html.parser')
     found = soup.findAll('a')
-    hrefs = []
     for link in found:
         hrefs.append(link['href'])
-    for i in range(len(hrefs)):
-        if re.search(r"\barchiveofourown.org/works\W", hrefs[i]) is not None:
-            ao3_list.append(hrefs[i])
-    for i in range(len(hrefs)):
-        if re.search(r"\barchiveofourown.org/chapters\W", hrefs[i]) is not None:
-            ao3_list.append(hrefs[i])
+
+    if re.search(r"\bseries\b", url) is not None:  # if the query has series
+        for i in range(len(hrefs)):
+            if re.search(r"\barchiveofourown.org\W", hrefs[i]) is not None:
+                ao3_list.append(hrefs[i])
+    else:
+        for i in range(len(hrefs)):
+            if re.search(r"\barchiveofourown.org/works/\b", hrefs[i]) is not None:
+                ao3_list.append(hrefs[i])
+            if re.search(r"\barchiveofourown.org/chapters/\b", hrefs[i]) is not None:
+                ao3_list.append(hrefs[i])
     if not ao3_list:
         return None
+
     ao3_url = re.search(
-        r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", ao3_list[0])
-    ao3_page = requests.get(ao3_url.group(0))
-    ao3_soup = BeautifulSoup(ao3_page.content, 'html.parser')
-    ao3_list_clean = (ao3_soup.find(
-        'li', attrs={'class': 'share'}).find('a', href=True))['href']
-    for i in range(len(ao3_list_clean)):
-        if "/" in ao3_list_clean[i]:
-            pos_of_slash1.append(i) 
-    for i in range(len(ao3_list_clean)):
-        if "?" in ao3_list_clean[i]:
-            pos_of_qmark = i
-    if pos_of_qmark is not None: 
-        # if ? is found in the ao3 url, extract the story id by appending the characters between 2nd / and ?
-        for i in range(pos_of_slash1[1]+1, pos_of_qmark):
-            ao3_id_list1.append(ao3_list_clean[i])
-    else: # extract the story id by appending the characters between 2nd and 3rd /
-        for i in range(pos_of_slash1[1]+1, pos_of_slash1[2]):
-            ao3_id_list1.append(ao3_list_clean[i])
-    ao3_id = ''.join(ao3_id_list1)
+        r"https?:\/\/(www/.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*", ao3_list[0])
+
+    if re.search(r"archiveofourown.org/series\W", ao3_url.group(0)) is None:
+        ao3_page = requests.get(ao3_url.group(0))  # , verify=False)
+        ao3_soup = BeautifulSoup(ao3_page.content, 'html.parser')
+
+        ao3_list_clean = (ao3_soup.find(
+            'li', attrs={'class': 'share'}).find('a', href=True))['href']
+
+        for i in range(len(ao3_list_clean)):
+            if "/" in ao3_list_clean[i]:
+                pos_of_slash1.append(i)
+
+        for i in range(len(ao3_list_clean)):
+            if "?" in ao3_list_clean[i]:
+                pos_of_qmark = i
+
+        if pos_of_qmark is not None:
+            # if ? is found in the ao3 url, extract the story id by appending the characters between 2nd / and ?
+            for i in range(pos_of_slash1[1]+1, pos_of_qmark):
+                ao3_id_list1.append(ao3_list_clean[i])
+
+        else:  # extract the story id by appending the characters between 2nd and 3rd /
+            for i in range(pos_of_slash1[1]+1, pos_of_slash1[2]):
+                ao3_id_list1.append(ao3_list_clean[i])
+        ao3_id = ''.join(ao3_id_list1)
+
+    else:
+        ao3_id = ao3_url.group(0)
     return ao3_id
+
+
+def get_ffn_id(query):
+    ffn_list = []
+    pos_of_slash1 = []
+    ffn_id_list1 = []
+    hrefs = []
+
+    url = 'https://www.google.com/search?q=' + \
+        query+"+fanfiction"
+    page = requests.get(url)  # , verify=False)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    found = soup.findAll('a')
+
+    for link in found:
+        hrefs.append(link['href'])
+
+    for i in range(len(hrefs)):
+        if re.search(r"fanfiction.net\W", hrefs[i]) is not None:
+            ffn_list.append(hrefs[i])
+
+    if not ffn_list:
+        return
+
+    ffn_url = re.search(
+        r"https?:\/\/(www/.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*", ffn_list[0])
+    ffn_list_clean = list(ffn_url.group(0))
+
+    for i in range(len(ffn_list_clean)):
+        if "/" in ffn_list_clean[i]:
+            pos_of_slash1.append(i)
+
+    for i in range(pos_of_slash1[3]+1, pos_of_slash1[4]):
+        ffn_id_list1.append(ffn_list_clean[i])
+
+    ffn_id = ''.join(ffn_id_list1)
+    return ffn_id
