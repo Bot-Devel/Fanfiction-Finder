7d89a1fcdeb2ac059c23bec7723b7a9f87648813
arzkar <roguedevone@gmail.com>
2021-02-08 20:56:45 +0530

    Added fallback for ffnet connection error and added author name & url to the embed
    - main.py: Removed auto-deletion of messages since the fallback for ffnet & ao3 is present
    - metadata.py: Added set_author() to the embed for author name, profile url and icon
    - metadata_processing.py: Added ao3_author_url since it is needed for set_author()

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import re
 import discord
-import datetime
 from dotenv import load_dotenv
+import datetime
+
 from utils.metadata import ao3_metadata, ffn_metadata
 from utils.bot_status import keep_alive
 client = discord.Client()
@@ -17,13 +18,12 @@ async def on_message(message):
     if message.author == client.user:
         return  # None
     msg = list(message.content.lower())
-    do_not_delete = 0
 
     channel = ['803746153296429066', '752196383066554538',
                '801841616020570112', '801842614559047771']
-
     whitelist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'é',
                  'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', ' ', '.', ';', ',', '"', "'", '`', '…', '*', '-', ':', '/', '%', '#']
+
     if str(message.channel.id) in channel:
         if all(elem in whitelist for elem in msg):  # if msg in whitelist
             if re.search(r"^ao3\b", message.content.lower()) is not None:
@@ -33,10 +33,10 @@ async def on_message(message):
                 if embed_pg is None:  # if not found in ao3, search in ffn
                     embed_pg = ffn_metadata(msg)
 
-                # if embed_pg is None:
-                #     embed_pg = discord.Embed(
-                #         description="Fanfiction not found!"
-                #     )
+                if embed_pg is None:
+                    embed_pg = discord.Embed(
+                        description="Fanfiction not found!"
+                    )
             elif re.search(r"^ffn\b", message.content.lower()) is not None:
                 msg = message.content.replace("ffn", "")
                 embed_pg = ffn_metadata(msg)
@@ -44,46 +44,42 @@ async def on_message(message):
                 if embed_pg is None:  # if not found in ffn, search in ao3
                     embed_pg = ao3_metadata(msg)
 
-                # if embed_pg is None:
-                #     embed_pg = discord.Embed(
-                #         description="Fanfiction not found!"
-                #     )
+                if embed_pg is None:
+                    embed_pg = discord.Embed(
+                        description="Fanfiction not found!"
+                    )
+
             elif re.search(r"`(.*?)`", message.content.lower()) is not None:
-                do_not_delete = 1
                 msg = re.search(r"`(.*?)`", message.content.lower())
-                embed_pg = ao3_metadata(msg.group(1))
+                embed_pg = ffn_metadata(msg.group(1))
+
+                if embed_pg is None:  # if not found in ffn, search in ao3
+                    msg2 = msg.group(1).replace("ao3", "")
+                    embed_pg = ao3_metadata(msg2)
 
                 if embed_pg is None:
-                    msg2 = msg.group(1).replace("ffn", "")
-                    embed_pg = ffn_metadata(msg2)
-
-                # if embed_pg is None:
-                #     embed_pg = discord.Embed(
-                #         description="Fanfiction not found!"
-                #     )
+                    embed_pg = discord.Embed(
+                        description="Fanfiction not found!"
+                    )
 
             elif re.search(r"https?:\/\/(www.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&=]*", message.content.lower()) is not None:
-                do_not_delete = 1
                 if re.search(r"fanfiction.net\b",  message.content) is not None:
                     embed_pg = ffn_metadata(message.content)
-                elif re.search(r"archiveofourown.org\b", message.content) is not None:
+
+                if re.search(r"archiveofourown.org\b", message.content) is not None:
                     # if not found in ffn, search in ao3
                     embed_pg = ao3_metadata(message.content)
-                    embed_pg.timestamp = datetime.datetime.utcnow()
 
-                # if embed_pg is None:
-                #     embed_pg = discord.Embed(
-                #         description="Fanfiction not found!"
-                #     )
-            if embed_pg is None:
-                if do_not_delete == 0:  # Delete msg only when the msg doesnt contain url
-                    await message.delete()
-            else:
-                embed_pg.set_footer(text="Searched by " + str(message.author))
-                embed_pg.timestamp = datetime.datetime.utcnow()
+                elif embed_pg is None:
+                    embed_pg = discord.Embed(
+                        description="Fanfiction not found!"
+                    )
+
+            embed_pg.set_footer(text="Searched by " +
+                                str(message.author))
+            embed_pg.timestamp = datetime.datetime.utcnow()
 
             await message.channel.send(embed=embed_pg)
-            if do_not_delete == 0:  # Delete msg only when the msg doesnt contain url
-                await message.delete()
+
 keep_alive()
 client.run(TOKEN)
--- utils/metadata_processing.py
+++ utils/metadata_processing.py
@@ -14,6 +14,9 @@ def ao3_metadata_works(ao3_url):
     ao3_author_name = (ao3_soup.find(
         'h3', attrs={'class': 'byline heading'}).find('a').contents[0]).strip()
 
+    ao3_author_url = ao3_soup.find(
+        'h3', attrs={'class': 'byline heading'}).find('a', href=True)['href']
+
     ao3_story_summary = ao3_soup.find(
         'div', attrs={'class': 'summary module'}).find(
         'blockquote', attrs={'class': 'userstuff'}).text
@@ -51,11 +54,12 @@ def ao3_metadata_works(ao3_url):
     ao3_story_length = "{:,}".format(int(ao3_story_length))
     ao3_story_chapters = ao3_story_chapter_clean(ao3_story_chapters)
     ao3_story_last_up = story_last_up_clean(ao3_story_last_up)
+    ao3_author_url = "https://archiveofourown.org"+ao3_author_url
 
     if len(list(ao3_story_summary)) > 2048:
         ao3_story_summary = ao3_story_summary[:2030] + "..."
 
-    return ao3_story_name, ao3_author_name, ao3_story_summary, ao3_story_status, ao3_story_last_up, ao3_story_length, ao3_story_chapters
+    return ao3_story_name, ao3_author_name, ao3_author_url, ao3_story_summary, ao3_story_status, ao3_story_last_up, ao3_story_length, ao3_story_chapters
 
 
 def ao3_metadata_series(ao3_url):
@@ -69,6 +73,9 @@ def ao3_metadata_series(ao3_url):
     ao3_author_name = (ao3_soup.find(
         'div', attrs={'class': 'series-show region'}).find(
         'a', attrs={'rel': 'author'}).contents[0]).strip()
+
+    ao3_author_url = ao3_soup.find('div', attrs={'class': 'series-show region'}).find(
+        'dt', text='Creator:').findNext('dd').find('a', href=True)['href']
 
     ao3_series_summary = ao3_soup.find(
         'div', attrs={'class': 'series-show region'}).find(
@@ -112,14 +119,14 @@ def ao3_metadata_series(ao3_url):
 
     ao3_series_works_index = get_ao3_series_works_index(ao3_soup)
     ao3_series_last_up = story_last_up_clean(ao3_series_last_up)
+    ao3_author_url = "https://archiveofourown.org"+ao3_author_url
 
     if len(list(ao3_series_summary)) > 2048:
         ao3_series_summary = ao3_series_summary[:1930] + "..."
     else:
         ao3_series_summary = ao3_series_summary + \
             '\n\n'+"📚 **Works**:\n"+ao3_series_works_index
-
     if len(list(ao3_series_summary)) > 2048:  # recheck the size of summary
         ao3_series_summary = ao3_series_summary[:1930] + "..."
 
-    return ao3_series_name, ao3_author_name, ao3_series_summary, ao3_series_status, ao3_series_last_up, ao3_series_length, ao3_series_works
+    return ao3_series_name, ao3_author_name, ao3_author_url, ao3_series_summary, ao3_series_status, ao3_series_last_up, ao3_series_length, ao3_series_works
