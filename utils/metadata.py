c7d4c99846aa3d479642b4ae71fed850c2fe54cb
arzkar <roguedevone@gmail.com>
2021-02-22 21:07:12 +0530

    Refactored the code by removing redundant parts

--- utils/metadata.py
+++ utils/metadata.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 import cloudscraper
 
-from utils.search import get_ao3_id, get_ffn_id
+from utils.search import get_ao3_url, get_ffn_url
 from utils.processing import story_last_up_clean, ffn_process_details, ao3_convert_chapters_to_works
 from utils.metadata_processing import ao3_metadata_works, ao3_metadata_series
 
@@ -12,69 +12,26 @@ def ao3_metadata(query):
     if re.search(r"https?:\/\/(www/.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*", query) is None:
 
         query = query.replace(" ", "+")
-        ao3_id = get_ao3_id(query)
+        ao3_url = get_ao3_url(query)
 
-        if not ao3_id:
-            embed = None
-            return embed
+    else:  # if the query is an url, no need to call get_ao3_url
+        ao3_url = query
 
-        if ao3_id == 1:
-            embed = None
-            return embed
+    if ao3_url is None:
+        return None
 
-        # its an ao3 series, get_ao3_id will return the ao3 series url
-        if re.search(r"https?:\/\/(www/.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*", ao3_id) is not None:
-            ao3_url = ao3_id  # ao3_url was returned inplace of ao3_id
-            ao3_series_name, ao3_author_name, ao3_author_url, ao3_series_summary, ao3_series_status, ao3_series_last_up, ao3_series_length, ao3_series_works = ao3_metadata_series(
-                ao3_url)
-
-            embed = discord.Embed(
-                title=ao3_series_name,
-                url=ao3_url,
-                description=ao3_series_summary,
-                colour=discord.Colour(0x272b28))
-
-            if ao3_series_status == "Completed":
-
-                embed.add_field(
-                    name='**📜 Last Updated:**',
-                    value=ao3_series_last_up +
-                    " - "+ao3_series_status, inline=True)
-
-            elif ao3_series_status == "Updated":
-
-                embed.add_field(
-                    name='**📜 Last Updated:**',
-                    value=ao3_series_last_up, inline=True)
-
-            embed.add_field(
-                name='**📖 Length:**',
-                value=ao3_series_length +
-                " words in "+ao3_series_works+" works", inline=True)
-
-            embed.set_author(name=ao3_author_name, url=ao3_author_url,
-                             icon_url="https://archiveofourown.org/images/ao3_logos/logo_42.png")
-
-            return embed
-        else:
-            ao3_url = "https://archiveofourown.org/works/"+''.join(ao3_id)
-    else:  # if the query was ao3 url, not get_fic_id needed
-        ao3_url = re.search(
-            r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*\b", query)
-        ao3_url = ao3_url.group()
-
-    if re.search(r"archiveofourown.org/works/\b", ao3_url) is not None:
-        ao3_story_name, ao3_author_name, ao3_author_url, ao3_story_summary, ao3_story_status, ao3_story_last_up, ao3_story_length, ao3_story_chapters = ao3_metadata_works(
-            ao3_url)
-
-    elif re.search(r"archiveofourown.org/chapters/\b", ao3_url) is not None:
+    if re.search(r"/chapters/\b", ao3_url) is not None:
         ao3_url = ao3_convert_chapters_to_works(
             ao3_url)  # convert the url from /chapters/ to /works/
 
         ao3_story_name, ao3_author_name, ao3_author_url, ao3_story_summary, ao3_story_status, ao3_story_last_up, ao3_story_length, ao3_story_chapters = ao3_metadata_works(
             ao3_url)
 
-    elif re.search(r"archiveofourown.org/series/\b", ao3_url) is not None:
+    elif re.search(r"/works/\b", ao3_url) is not None:
+        ao3_story_name, ao3_author_name, ao3_author_url, ao3_story_summary, ao3_story_status, ao3_story_last_up, ao3_story_length, ao3_story_chapters = ao3_metadata_works(
+            ao3_url)
+
+    elif re.search(r"/series/\b", ao3_url) is not None:
         ao3_series_name, ao3_author_name, ao3_author_url, ao3_series_summary, ao3_series_status, ao3_series_last_up, ao3_series_length, ao3_series_works = ao3_metadata_series(
             ao3_url)
 
@@ -143,19 +100,13 @@ def ffn_metadata(query):
             embed = None
             return embed
         query = query.replace(" ", "+")
-        ffn_id = get_ffn_id(query)
+        ffn_url = get_ffn_url(query)
 
-        if not ffn_id:
-            embed = None
-            return embed
-
-        if ffn_id == 1:
-            embed = None
-            return embed
-
-        ffn_url = "https://www.fanfiction.net/s/"+''.join(ffn_id)
-    else:  # if the query was ffn url, not get_fic_id needed
+    else:  # if the query was ffn url, no need to call get_ffn_url
         ffn_url = query
+
+    if ffn_url is None:
+        return None
 
     scraper = cloudscraper.create_scraper()
     ffn_page = scraper.get(ffn_url).text
@@ -180,6 +131,7 @@ def ffn_metadata(query):
 
         ffn_story_last_up = story_last_up_clean(ffn_story_last_up)
         ffn_author_url = "https://www.fanfiction.net"+ffn_author_url
+        
         if len(list(ffn_story_summary)) > 2048:
             ffn_story_summary = ffn_story_summary[:2030] + "..."
 
