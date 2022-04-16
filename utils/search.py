baddec50537916d165f040a9b5602837ee4164a3
arzkar <arzkar.dev@gmail.com>
2022-04-16 22:38:14 +0530

    Added alternative search engine: Bing
    If google returns errors like 429s, the bot will retry using Bing.

--- utils/search.py
+++ utils/search.py
@@ -5,50 +5,56 @@ from loguru import logger
 
 
 URL_VALIDATE = r"(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?"
+search_engines = ['google', 'bing']
 
 
 def get_ao3_url(query):
     ao3_list = []
     href = []
 
-    url = 'https://www.google.com/search?q=' + \
-        query+"+ao3"
+    for engine in search_engines:
+        url = f'https://www.{engine}.com/search?q={query}+ao3'
 
-    page = requests.get(url)
-    logger.debug(f"GET: {page.status_code}: {url}")
+        page = requests.get(url)
+        logger.debug(f"GET: {page.status_code}: {url}")
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    found = soup.findAll('a')
-    for link in found:
-        href.append(link['href'])
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            found = soup.findAll('a')
+            for link in found:
+                href.append(link['href'])
 
-    if re.search(r"\bseries\b", url):  # if the query has series
-        for i in range(len(href)):
-            if re.search(r"\barchiveofourown.org/series/\b", href[i]) is not None:
-                logger.info(f"URL FOUND: {href[i]}")
-                ao3_list.append(href[i])
+            if re.search(r"\bseries\b", url):  # if the query has series
+                for i in range(len(href)):
+                    if re.search(r"\barchiveofourown.org/series/\b", href[i]) is not None:
+                        logger.info(f"URL FOUND: {href[i]}")
+                        ao3_list.append(href[i])
 
-    else:  # if query is unspecified
-        for i in range(len(href)):
+            else:  # if query is unspecified
+                for i in range(len(href)):
 
-            # append /works/ first
-            if re.search(r"\barchiveofourown.org/works/\b", href[i]) is not None:
-                logger.info(f"URL FOUND: {href[i]}")
-                ao3_list.append(href[i])
+                    # append /works/ first
+                    if re.search(r"\barchiveofourown.org/works/\b", href[i]) is not None:
+                        logger.info(f"URL FOUND: {href[i]}")
+                        ao3_list.append(href[i])
 
-            # append /chapters/ next
-            # if re.search(r"\barchiveofourown.org/chapters/\b", href[i]) is not None:
-            #     logger.info(f"URL FOUND: {href[i]}")
-            #     ao3_list.append(href[i])
+                    # append /chapters/ next
+                    # if re.search(r"\barchiveofourown.org/chapters/\b", href[i]) is not None:
+                    #     logger.info(f"URL FOUND: {href[i]}")
+                    #     ao3_list.append(href[i])
 
-            # append /series/ next
-            if re.search(r"\barchiveofourown.org/series/\b", href[i]) is not None:
-                logger.info(f"URL FOUND: {href[i]}")
-                ao3_list.append(href[i])
+                    # append /series/ next
+                    if re.search(r"\barchiveofourown.org/series/\b", href[i]) is not None:
+                        logger.info(f"URL FOUND: {href[i]}")
+                        ao3_list.append(href[i])
 
-    if not ao3_list:
-        logger.error("URL NOT FOUND")
-        return None
+            if not ao3_list:
+                logger.error(f"URL NOT FOUND using {engine}")
+                continue
+            else:
+                break
+        else:
+            continue
 
     # extract the https url from the the string since it contains /url?q=
     ao3_url = re.search(URL_VALIDATE, ao3_list[0])
@@ -59,27 +65,29 @@ def get_ao3_url(query):
 def get_ffn_url(query):
     ffn_list = []
     href = []
+    for engine in search_engines:
+        url = f'https://www.{engine}.com/search?q={query}+fanfiction'
+        page = requests.get(url)
+        logger.debug(f"GET: {page.status_code}: {url}")
 
-    url = 'https://www.google.com/search?q=' + \
-        query+"+fanfiction"
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            found = soup.findAll('a')
 
-    page = requests.get(url)
-    logger.debug(f"GET: {page.status_code}: {url}")
+            for link in found:
+                href.append(link['href'])
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    found = soup.findAll('a')
-
-    for link in found:
-        href.append(link['href'])
-
-    for i in range(len(href)):
-        if re.search(r"fanfiction.net/s/", href[i]) is not None:
-            logger.info(f"URL FOUND: {href[i]}")
-            ffn_list.append(href[i])
-
-    if not ffn_list:
-        logger.error("URL NOT FOUND")
-        return
+            for i in range(len(href)):
+                if re.search(r"fanfiction.net/s/", href[i]) is not None:
+                    logger.info(f"URL FOUND: {href[i]}")
+                    ffn_list.append(href[i])
+            if not ffn_list:
+                logger.error(f"URL NOT FOUND using {engine}")
+                continue
+            else:
+                break
+        else:
+            continue
 
     ffn_url = re.search(URL_VALIDATE, ffn_list[0])
 
