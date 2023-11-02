b020a103e92039522e660ead964316aeb3dc9a7c
Arbaaz Laskar <arzkar.dev@gmail.com>
2023-11-03 00:31:03 +0530

    chore: cleanup deps & update .env.ex

--- .env.ex
+++ .env.ex
@@ -2,4 +2,4 @@
 DISCORD_TOKEN = <your-bot-token>
 BOT_ID = <bot-id>
 OWNER_ID = <your-id>
-FICHUB_SITES = ""
+FICHUB_SITES = "fanfiction.net/, https://archiveofourown.org/, https://www.hpfanficarchive.com/, https://fictionalley.blogspot.com/, https://www.fanficauthors.net/, https://portkey-archive.org/, http://www.siye.co.uk/, https://www.fictionpress.com/, http://fictionhunt.com/, https://forums.spacebattles.com/, https://forums.sufficientvelocity.com/, https://forum.questionablequesting.com/, https://harrypotterfanfiction.com/, https://www.parahumans.net/, http://www.adult-fanfiction.org/, http://fanfics.me/, https://royalroadl.com/, https://wertifloke.wordpress.com/, http://www.sugarquill.net/, https://forums.bulbagarden.net/index.php, https://thefanfictionforum.net/xenforo/index.php, https://www.fanficparadise.com/fpforum-sfw/index.php, https://www.fanficparadise.com/fpforum-nsfw/index.php, https://wanderinginn.com/"
