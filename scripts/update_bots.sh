fc91e165b5dc0caab5840de70500968435ed0a13
Arbaaz Laskar <arzkar.dev@gmail.com>
2023-11-05 20:55:39 +0530

    chore: Added the update script for my VPS

---
+++ scripts/update_bots.sh
@@ -0,0 +1,17 @@
+# Script to update the bots and restart the services in the VPS
+
+# Updating the bots
+cd /home/arbaaz/Projects/Fanfiction-Finder
+git reset --hard HEAD~1
+git pull
+
+cd /home/arbaaz/Projects/Quote-Finder
+git reset --hard HEAD~1
+git pull
+
+# Updating the services
+sudo cp /home/arbaaz/Projects/Quote-Finder/Quote-Finder.service  /etc/systemd/system/Quote-Finder.service
+sudo cp /home/arbaaz/Projects/Fanfiction-Finder/Fanfiction-Finder.service  /etc/systemd/system/Fanfiction-Finder.service
+sudo systemctl daemon-reload
+sudo systemctl restart /etc/systemd/system/Fanfiction-Finder.service
+sudo systemctl restart /etc/systemd/system/Quote-Finder.service
