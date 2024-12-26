6b19d2a842657770fb1d50d43bc8d3a4814d62db
sfu69 <65554180+sfu69@users.noreply.github.com>
2024-12-27 00:23:04 +0100

    Added emoji check

--- main.py
+++ main.py
@@ -40,9 +40,11 @@ class FicFinder(commands.Bot):
         message = await channel.fetch_message(payload.message_id)
         
         # Check if the message was sent by the bot
-        if message.author == self.user:
+        if message.author == self.user and payload.emoji.name == "👎":
             await message.delete()
-
+        else:
+            await message.remove_reaction(payload.emoji, payload.member)
+            
     async def on_message(self, message: discord.Message) -> None:
         """Command to search and find the fanfiction by scraping google"""
         # TODO: Re-evaluate logging for messages containing "-log".
